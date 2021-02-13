8b6dc73be98e08aca91295e04a749b6499e993bb
Felix Akindele <akindelefelix1@gmail.com>
2021-02-13 21:20:47 +0100

    init

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We are the food tech!!</Text>
       <StatusBar style="auto" />
     </View>
   );
