cbce8699328efe66c48186cb36f1cde9b9552839
Felix Akindele <akindelefelix1@gmail.com>
2021-02-27 01:52:19 +0100

    cleam

--- App.js
+++ App.js
@@ -1,27 +1,10 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 
-
-import MainComponent from "./Components/MainComponent";
-
-
-// const App = createAppContainer(MainNavigator, {
-//   initialRouteName: "Veggiepe",
-//   defaultNavigationOptions: {
-//     headerStyle: {
-//       backgroundColor: "#f4511e"
-//     },
-//     headerTintColor: "#fff",
-//     headerTitleStyle: {
-//       fontWeight: "bold"
-//     }
-//   }
-// });
-
 const App = () => {
   return (
     <NavigationContainer>
-      <MainComponent />
+      
     </NavigationContainer>
   )
 }
