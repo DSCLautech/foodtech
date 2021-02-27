29ada492fda6a40f79184368de11828e7f425509
olawale juwon <olawalejuwon@gmail.com>
2021-02-26 18:32:11 -0600

    refactor

--- Components/MainComponent.js
+++ Components/MainComponent.js
@@ -17,6 +17,17 @@ export default function MainComponent() {
 
     return (
         <Stack.Navigator
+            screenOptions={
+                {
+                    headerStyle: {
+                        backgroundColor: "#f4511e"
+                    },
+                    headerTintColor: "#fff",
+                    headerTitleStyle: {
+                        fontWeight: "bold"
+                    }
+                }
+            }
         >
             <Stack.Screen
                 name="Home"
