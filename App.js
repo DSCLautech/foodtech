daec28a85dd7003b8348d799e64b8698212380e2
olawale juwon <olawalejuwon@gmail.com>
2021-02-26 18:22:46 -0600

    done

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { Home } from "./src/views/Home";
-import { List } from "./src/views/List";
-import { Recipe } from "./src/views/Recipe";
+import { NavigationContainer } from '@react-navigation/native';
 
-const MainNavigator = createStackNavigator({
-  Home: { screen: Home },
-  List: { screen: List },
-  Recipe: { screen: Recipe }
-});
 
-const App = createAppContainer(MainNavigator, {
-  initialRouteName: "Veggiepe",
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: "#f4511e"
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: "bold"
-    }
-  }
-});
+import MainComponent from "./Components/MainComponent";
+
+
+// const App = createAppContainer(MainNavigator, {
+//   initialRouteName: "Veggiepe",
+//   defaultNavigationOptions: {
+//     headerStyle: {
+//       backgroundColor: "#f4511e"
+//     },
+//     headerTintColor: "#fff",
+//     headerTitleStyle: {
+//       fontWeight: "bold"
+//     }
+//   }
+// });
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MainComponent />
+    </NavigationContainer>
+  )
+}
 
 export default App;
--- Components/MainComponent.js
+++ Components/MainComponent.js
@@ -2,11 +2,31 @@ import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { View } from 'react-native'
 import BottomNav from './BottomNavComponent/BottomNav'
+import { createStackNavigator } from '@react-navigation/stack';
+import { List } from '../src/views/List';
+import { Home } from '../src/views/Home';
+import { Recipe } from '../src/views/Recipe';
+const Stack = createStackNavigator();
 
 export default function MainComponent() {
+    // return (
+    //     <View>
+    //         <BottomNav />
+    //     </View>
+    // )
+
     return (
-        <View>
-            <BottomNav />
-        </View>
+        <Stack.Navigator
+        >
+            <Stack.Screen
+                name="Home"
+                component={Home} />
+            <Stack.Screen
+                name="List"
+                component={List} />
+            <Stack.Screen
+                name="Recipe"
+                component={Recipe} />
+        </Stack.Navigator>
     )
 }
